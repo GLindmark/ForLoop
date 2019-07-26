3509da86fbd5df206a914bcf28691970b5bc253f
lindmark421@gmail.com <lindmark421@gmail.com>
2019-07-26 11:59:31 -0400

    Concat names in all caps

--- ForLoopProject/Program.cs
+++ ForLoopProject/Program.cs
@@ -4,23 +4,39 @@ namespace ForLoopProject {
     class Program {
         static void Main(string[] args) {
 
-            int[] ints = { 89 , 188, 295, 943, 975, 277, 119,
-                736, 952, 321, 521, 444, 758, 397,
-                915, 878, 674, 701, 379, 331, 804,
-                461, 459, 908, 373 };
+            //int[] ints = { 89 , 188, 295, 943, 975, 277, 119,
+            //    736, 952, 321, 521, 444, 758, 397,
+            //    915, 878, 674, 701, 379, 331, 804,
+            //    461, 459, 908, 373 };
 
-            int total = 0;
+            string[] names = {
+                "Brailee", "Brianna", "Marianne", "Alex", "Jesse", "Titus", "Maggie", "Glenn"
+            };
 
-            foreach(int anInt in ints) {
-                total += anInt;
+            string allNames = "";// create an "empty" string variable
+
+
+            foreach (string name in names) {
+
+                string upperName = name.ToUpper();// sets all of the names to all UPPERCASE
+
+                allNames += upperName;// this will take the value of allnames and 'concatenate' it to upperName
+
+            }
+            Console.WriteLine(allNames);
+        }
+            
+            //int total = 0;
+
+            //foreach(int anInt in ints) {
+            //    total += anInt;
             }
 
             //for(int idx = 0; idx< ints.Length; idx++) {
             //    total += ints[idx] *3 / 4;
             //}
 
-            decimal average = total / (decimal) ints.Length;// putting 'decimal' infront is a CAST which returns a decimal
-            Console.WriteLine($"Total; {total} , Average: {average}");
+            //decimal average = total / (decimal) ints.Length;// putting 'decimal' infront is a CAST which returns a decimal
+            //Console.WriteLine($"Total; {total} , Average: {average}");
         }
-    }
-}
+    
