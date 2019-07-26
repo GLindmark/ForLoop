d701d36157eebfa394f2381f0dff74ed3ee354a1
lindmark421@gmail.com <lindmark421@gmail.com>
2019-07-26 11:01:51 -0400

    did division of amounts

--- ForLoopProject/Program.cs
+++ ForLoopProject/Program.cs
@@ -4,7 +4,19 @@ namespace ForLoopProject {
     class Program {
         static void Main(string[] args) {
 
-            int[] ints = { };
+            int[] ints = { 89, 188, 295, 943, 975, 277, 119,
+                736, 952, 321, 521, 444, 758, 397,
+                915, 878, 674, 701, 379, 331, 804,
+                461, 459, 908, 373 };
+
+            int total = 0;
+
+            for(int idx = 0; idx< ints.Length; idx++) {
+                total += ints[idx] *3 / 4;
+            }
+
+            decimal average = total / (decimal) ints.Length;// putting 'decimal' infront is a CAST which returns a decimal
+            Console.WriteLine($"Total; {total} , Average: {average}");
         }
     }
 }
